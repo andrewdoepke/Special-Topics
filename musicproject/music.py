59cb456aceeb04a02c81710156d3bdf422ff198e
Andrew Doepke <andrewdoepke@outlook.com>
2021-04-28 11:35:01 -0500

    Started some stuff

--- musicproject/music.py
+++ musicproject/music.py
@@ -1,1 +1,10 @@
 import pandas as pd
+import matplotlib as plt
+import numpy as np
+
+#load in CSV files
+top10 = pd.read_csv("top10s.csv", index_col=0)
+megas = pd.read_csv("spotify_mega.csv", index_col=0)
+
+print(megas.head())
+
