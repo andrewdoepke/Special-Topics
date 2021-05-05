74ffc1509e61279a0c1939b7ef171f3ccd2da044
Andrew Doepke <andrewdoepke@outlook.com>
2021-05-04 19:52:02 -0500

    User Input
    yuh

--- musicproject/music.py
+++ musicproject/music.py
@@ -1,21 +1,52 @@
 import pandas as pd
 import recommender as r
+import difflib as dl
+import random
 
-#load in CSV files
-top10 = pd.read_csv("top10s.csv", index_col=0)
-megas = pd.read_csv("spotify_mega.csv", index_col=0)
 
-alldf = pd.DataFrame(pd.concat([top10, megas]).reset_index(drop=True))
-songy = alldf.iloc[2]
+def closest_title(uinp, title): #find index of title in the titles array closest to user input
+    stri = dl.get_close_matches(uinp, title, n=1) #get closest matched word to input
+    ind = 0
+    for s in title: #figure out index of this based on if the word is in the title
+        if s in stri:
+            return ind
+        ind += 1
+    return None
 
-recc = r.recommender()
+def load_data(): #load the datafiles
+    # load in CSV files
+    top10 = pd.read_csv("top10s.csv", index_col=0)
+    megas = pd.read_csv("spotify_mega.csv", index_col=0)
 
-recs = recc.recommend(data=alldf, song=songy)
+    #merge dataframes and reset the index
+    alldf = pd.DataFrame(pd.concat([top10, megas]).reset_index(drop=True))
 
-#print(recs[['title', 'artist']].to_string(index=False))
-print("\nSome songs you may like based on the song", songy.title, "by", songy.artist, "are:")
-for r in recs.iterrows():
-    print("     ",r[1]['title'], "by", r[1]['artist'])
+    #return this dataframe
+    return alldf
+
+def song_in(alldf): #take user input
+    titles = alldf['title'].array #grab titles of the dataframe
+    uinput = input("Enter the name of a song as best as you can:\nIf you would like recommendations from a random song, just press enter.\n")
+    songind = closest_title(uinput, titles) #find index of song based on input
+    if songind is not None:
+        return alldf.iloc[songind] #return song requested
+    else:
+        return alldf.iloc[random.randrange(len(titles))] #return random song
+
+def printresults(songy, recs): #pretty print results
+    print("\nSome songs you may like based on the song", songy.title, "by", songy.artist, "are:")
+    for x in recs.iterrows():
+        print("     ", x[1]['title'], "by", x[1]['artist'])
+
+def recommend(): #controller function
+    df = load_data()
+    recc = r.recommender()
+    song = song_in(df)
+    recs = recc.recommend(data=df, song=song)
+    printresults(song, recs)
+
+#Main:
+recommend()
 
 
 
